273cf61de5b66477c2f42f4681de950fea1d365d
Connor McCormick <mrredcon@gmail.com>
2018-09-08 12:42:18 -0500

    fix a merge mistake

--- FalloutRPG/Constants/Exceptions.cs
+++ FalloutRPG/Constants/Exceptions.cs
@@ -25,6 +25,7 @@ namespace FalloutRPG.Constants
         public const string CHAR_INVALID_SPECIAL_NAME = "SPECIAL name is invalid.";
         public const string CHAR_NOT_ENOUGH_SPECIAL_POINTS = "Not enough SPECIAL points.";
         public const string CHAR_SPECIAL_POINTS_GOES_OVER_MAX = "Unable to add SPECIAL points because it will take skill above the max level.";
+        public const string CHAR_TOO_MANY = "You have reached the limit of characters per account.";
 
         // NPC Exception Message
         public const string NPC_CHAR_EXISTS = "A character with that name already exists!";
