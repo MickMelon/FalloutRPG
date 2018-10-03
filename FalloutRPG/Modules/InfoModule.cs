622a244376a41c0fad7384c5b0a80fac40febe47
Connor McCormick <mrredcon@gmail.com>
2018-10-02 19:36:36 -0500

    Fix $info command exception when Duke or Mrred is not present

--- FalloutRPG/Modules/InfoModule.cs
+++ FalloutRPG/Modules/InfoModule.cs
@@ -47,9 +47,19 @@ namespace FalloutRPG.Modules
                     author
                         .WithName("F.R.A.G.S.")
                         .WithIconUrl(Context.Client.CurrentUser.GetAvatarUrl().ToString());
-                })
-                .AddField("Developed By", $"{app.Owner.Mention} and {mrredUser.Mention}")
-                .AddField("Name and Artwork By", $"{dukeUser.Mention}")
+                });
+
+                if (mrredUser != null)
+                    builder.AddField("Developed By", $"{app.Owner.Mention} and {mrredUser.Mention}");
+                else
+                    builder.AddField("Developed By", $"{app.Owner.Mention} and mrred");
+
+                if (dukeUser != null)
+                    builder.AddField("Name and Artwork By", $"{dukeUser.Mention}");
+                else
+                    builder.AddField("Name and Artwork By", $"Duke");
+
+                builder
                 .AddField("Library", $"Discord.Net ({DiscordConfig.Version})")
                 .AddField("Runtime", $"{RuntimeInformation.FrameworkDescription} {RuntimeInformation.ProcessArchitecture} " +
                     $"({RuntimeInformation.OSDescription} {RuntimeInformation.OSArchitecture})")
