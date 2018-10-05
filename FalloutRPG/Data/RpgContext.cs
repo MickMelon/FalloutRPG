9f611d67fc3d76c0045a14e05e3b23424bac63b6
Connor McCormick <mrredcon@gmail.com>
2018-10-04 20:56:25 -0500

    Begin work on Effects, temporary buffs or debuffs that can be applied to Characters

--- FalloutRPG/Data/RpgContext.cs
+++ FalloutRPG/Data/RpgContext.cs
@@ -6,9 +6,10 @@ namespace FalloutRPG.Data
 {
     public class RpgContext : DbContext
     {
-        public DbSet<Character> Characters { get; set; }
         public DbSet<Player> Players { get; set; }
         public DbSet<Campaign> Campaigns { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Effect> Effects { get; set; }
 
         public RpgContext(DbContextOptions<RpgContext> options) : base(options)
         {
---
+++ FalloutRPG/Models/Effect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FalloutRPG.Models
+{
+    public class Effect : BaseModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public IList<EffectSpecial> SpecialEffects { get; set; }
+        public IList<EffectSkill> SkillEffects { get; set; }
+    }
+}
