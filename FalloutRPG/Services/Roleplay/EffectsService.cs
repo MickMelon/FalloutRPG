85e3e771d9aa56bf2232d44a5c36f015aedf11e2
Connor McCormick <mrredcon@gmail.com>
2018-10-06 14:31:24 -0500

    Comment out EffectsService, going to focus on items and new rolling system instead

--- FalloutRPG/Services/Roleplay/EffectsService.cs
+++ FalloutRPG/Services/Roleplay/EffectsService.cs
@@ -10,24 +10,24 @@ namespace FalloutRPG.Services.Roleplay
     public class EffectsService
     {
         //private readonly CharacterService _characterService;
-        private readonly SkillsService _skillsService;
-        private readonly SpecialService _specialService;
+        //private readonly SkillsService _skillsService;
+        //private readonly SpecialService _specialService;
 
-        private readonly IRepository<Effect> _effectsRepository;
+        //private readonly IRepository<Effect> _effectsRepository;
 
         // I now realize that in order for this to work, we're also gonna need a RemoveEffect method
         // now the problem, is that there isn't a Character.Effects list or anything, so do we just apply the
         // effect in reverse to remove it? Probably should go ahead and add a NonMapped effect list...
 
-        public void ApplyEffect(Character character, Effect effect)
-        {
-            foreach (var specialEffect in effect.SpecialEffects)
-                _specialService.SetSpecial(character,
-                    specialEffect.SpecialAttribute,
-                    _specialService.GetSpecial(character, specialEffect.SpecialAttribute) + specialEffect.EffectValue);
+        //public void ApplyEffect(Character character, Effect effect)
+        //{
+        //    foreach (var specialEffect in effect.SpecialEffects)
+        //        _specialService.SetSpecial(character,
+        //            specialEffect.SpecialAttribute,
+        //            _specialService.GetSpecial(character, specialEffect.SpecialAttribute) + specialEffect.EffectValue);
 
-            //foreach (var skillEffect in effect.SkillEffects)
-                //_skillsService.SetSkill()
-        }
+        //    //foreach (var skillEffect in effect.SkillEffects)
+        //        //_skillsService.SetSkill()
+        //}
     }
 }
