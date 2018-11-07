49be87584a32defb4284a4ad7db14c6d94898878
Connor McCormick <mrredcon@gmail.com>
2018-11-07 08:47:41 -0600

    Empty out EffectsService

--- FalloutRPG/Services/Roleplay/EffectsService.cs
+++ FalloutRPG/Services/Roleplay/EffectsService.cs
@@ -9,30 +9,9 @@ namespace FalloutRPG.Services.Roleplay
 {
     public class EffectsService
     {
-        //private readonly CharacterService _characterService;
-        //private readonly SkillsService _skillsService;
-        //private readonly SpecialService _specialService;
-
-        //private readonly IRepository<Effect> _effectsRepository;
-
-        // I now realize that in order for this to work, we're also gonna need a RemoveEffect method
-        // now the problem, is that there isn't a Character.Effects list or anything, so do we just apply the
-        // effect in reverse to remove it? Probably should go ahead and add a NonMapped effect list...
-
-        public double GetArmorClass(Character receiver)
+        public void ApplyEffect(Character character, Effect effect)
         {
-            throw new NotImplementedException();
+            
         }
-
-        //public void ApplyEffect(Character character, Effect effect)
-        //{
-        //    foreach (var specialEffect in effect.SpecialEffects)
-        //        _specialService.SetSpecial(character,
-        //            specialEffect.SpecialAttribute,
-        //            _specialService.GetSpecial(character, specialEffect.SpecialAttribute) + specialEffect.EffectValue);
-
-        //    //foreach (var skillEffect in effect.SkillEffects)
-        //        //_skillsService.SetSkill()
-        //}
     }
 }
