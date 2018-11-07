360757054e17ed30556997f40ebe599816c667ea
Connor McCormick <mrredcon@gmail.com>
2018-11-07 08:42:32 -0600

    Item now has an ICollection of Effects and a ToString()

--- FalloutRPG/Models/Items/Item.cs
+++ FalloutRPG/Models/Items/Item.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace FalloutRPG.Models
 {
     public abstract class Item : BaseModel
     {
+        public Item()
+        {
+            Effects = new List<Effect>();
+        }
+
         public string Name { get; set; }
         public string Description { get; set; }
 
@@ -12,5 +19,9 @@ namespace FalloutRPG.Models
         public double Weight { get; set; }
 
         public bool Equipped { get; set; }
+
+        public virtual ICollection<Effect> Effects { get; set; }
+
+        public override string ToString() => Name;
     }
 }
