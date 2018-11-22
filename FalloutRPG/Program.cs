0f76197642860611d93e99741b1162ad29562563
Connor McCormick <mrredcon@gmail.com>
2018-11-22 09:03:01 -0600

    Add ReliabilityService to mitigate disconnects that never reconnect

--- FalloutRPG/Program.cs
+++ FalloutRPG/Program.cs
@@ -68,6 +68,7 @@ namespace FalloutRPG
             .AddSingleton<LogService>()
             .AddSingleton<StartupService>()
             .AddSingleton<HelpService>()
+            .AddSingleton<ReliabilityService>()
 
             // Roleplay
             .AddSingleton<RollService>()
