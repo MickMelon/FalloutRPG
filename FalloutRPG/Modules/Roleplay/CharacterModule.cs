207a03b9fbcca5d1fa16d96ec879e4e3809c0bb6
Connor McCormick <mrredcon@gmail.com>
2018-11-25 11:43:50 -0600

    Allow users to input part of a character's name with ActivateCharacterAsync()

--- FalloutRPG/Modules/Roleplay/CharacterModule.cs
+++ FalloutRPG/Modules/Roleplay/CharacterModule.cs
@@ -100,7 +100,8 @@ namespace FalloutRPG.Modules.Roleplay
                 return;
             }
 
-            var charMatch = chars.Find(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            //var charMatch = chars.Find(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+            var charMatch = chars.Find(x => x.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
 
             if (charMatch == null)
             {
