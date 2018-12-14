ec73b3996968434e09913b676b03ad20989dd824
Connor McCormick <mrredcon@gmail.com>
2018-12-13 20:42:45 -0600

    Remove JsonProperty attribute

--- FalloutRPG/Models/Characters/Skill.cs
+++ FalloutRPG/Models/Characters/Skill.cs
@@ -7,7 +7,6 @@ namespace FalloutRPG.Models
 {
     public class Skill : Statistic
     {
-        [JsonProperty(PropertyName = "specialId")]
         public Special Special { get; set; }
 
         public int MinimumValue { get; set; }
