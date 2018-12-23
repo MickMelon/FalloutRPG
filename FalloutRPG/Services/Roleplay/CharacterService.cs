40b766fa24ea40001e3c4cf3b695df04e3153d70
Connor McCormick <mrredcon@gmail.com>
2018-12-23 16:53:19 -0600

    Chargen now gives the correct amount of Special Points

--- FalloutRPG/Services/Roleplay/CharacterService.cs
+++ FalloutRPG/Services/Roleplay/CharacterService.cs
@@ -84,7 +84,7 @@ namespace FalloutRPG.Services.Roleplay
                 Story = "",
                 Experience = 0,
                 ExperiencePoints = 0,
-                SpecialPoints = SpecialService.STARTING_SPECIAL_POINTS - SpecialService.SPECIAL_MIN * SpecialService.Specials.Count,
+                SpecialPoints = SpecialService.STARTING_SPECIAL_POINTS,
                 TagPoints = SkillsService.TAG_POINTS,
                 Money = 1000,
                 Statistics = new List<StatisticValue>(),
