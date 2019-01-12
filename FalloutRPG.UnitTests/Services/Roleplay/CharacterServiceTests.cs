582248af73bfaf0ba82a20bb6494444a0ab5dfbd
MickMelon <michael.mcmillan@outlook.com>
2019-01-12 00:44:14 +0000

    Add unit tests and update travis file to work with net core

--- FalloutRPG.UnitTests/Services/Roleplay/CharacterServiceTests.cs
+++ FalloutRPG.UnitTests/Services/Roleplay/CharacterServiceTests.cs
@@ -12,6 +12,7 @@ namespace FalloutRPG.UnitTests.Services.Roleplay
 {
     public class CharacterServiceTests
     {
+        #region GetCharacterAsync() Tests
         [Fact]
         public async Task GetCharacter_ValidDiscordIdActiveCharacter_ReturnCharacter()
         {
@@ -79,13 +80,15 @@ namespace FalloutRPG.UnitTests.Services.Roleplay
             // Assert
             Assert.Null(character);
         }
+        #endregion
 
+        #region GetAllCharactersAsync() Tests
         [Fact]
-        public async Task GetAllCharacters_ValidDiscordIdValidCharacters_ReturnAllCharacters()
+        public async Task GetAllCharacters_ValidDiscordIdMultipleCharacters_ReturnAllCharacters()
         {
             // Arrange
             var options = new DbContextOptionsBuilder<RpgContext>()
-                .UseInMemoryDatabase(databaseName: "ValidDiscordIdValidCharacters_ReturnAllCharacters")
+                .UseInMemoryDatabase(databaseName: "ValidDiscordIdMultipleCharacters_ReturnAllCharacters")
                 .Options;
             var context = new RpgContext(options);
             context.Add(new Character() { DiscordId = (ulong)1 }); 
@@ -101,7 +104,28 @@ namespace FalloutRPG.UnitTests.Services.Roleplay
             var characters = await charService.GetAllCharactersAsync(1);
 
             // Assert
-            Assert.True(characters != null && characters.Count == 3);
+            Assert.True(characters.Count == 3);
         }
+
+        [Fact]
+        public async Task GetAllCharacters_InvalidDiscordId_ReturnNull()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<RpgContext>()
+                .UseInMemoryDatabase(databaseName: "GetAllCharacters_InvalidDiscordId_ReturnNull")
+                .Options;
+            var context = new RpgContext(options);
+            var statsRepository = new EfSqliteRepository<Statistic>(context);
+            var charRepository = new EfSqliteRepository<Character>(context);
+            var statsService = new StatisticsService(statsRepository);
+            var charService = new CharacterService(statsService, charRepository);
+
+            // Act
+            var characters = await charService.GetAllCharactersAsync(1);
+
+            // Assert
+            Assert.True(characters.Count == 0);
+        }
+        #endregion
     }
 }
